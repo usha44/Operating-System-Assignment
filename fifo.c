f46ea6eea032c117d52df04f286050f39949f913
usha44 <dhakalusha524@gmail.com>
2026-07-14 17:04:48 +0000

    Add FIFO and LRU page replacement

--- fifo.c
+++ fifo.c
@@ -1,64 +1,79 @@
 #include <stdio.h>
 
-int main() {
-    int frames, pages, i, j;
-    int page[20], memory[10];
-
-    int hits = 0, faults = 0;
-    int pointer = 0;
-    int found;
-
-    printf("Enter number of frames: ");
-    scanf("%d", &frames);
-
-    printf("Enter number of pages: ");
-    scanf("%d", &pages);
-
-    printf("Enter page reference string:\n");
-    for(i = 0; i < pages; i++) {
-        scanf("%d", &page[i]);
-    }
-
-    for(i = 0; i < frames; i++)
-        memory[i] = -1;
+void fifo() {
+    int page[] = {9,8,0,8,7,6,7,5,3,7};
+    int frame[4] = {-1,-1,-1,-1};
+    int i, j, found, index = 0, hit = 0, fault = 0;
 
     printf("\nFIFO Page Replacement\n");
 
-    for(i = 0; i < pages; i++) {
-
+    for(i = 0; i < 10; i++) {
         found = 0;
 
-        for(j = 0; j < frames; j++) {
-            if(memory[j] == page[i]) {
+        for(j = 0; j < 4; j++)
+            if(frame[j] == page[i]) {
                 found = 1;
-                hits++;
-                break;
+                hit++;
+            }
+
+        if(!found) {
+            frame[index] = page[i];
+            index = (index + 1) % 4;
+            fault++;
+        }
+
+        printf("%d -> ", page[i]);
+        for(j = 0; j < 4; j++)
+            printf("%d ", frame[j]);
+
+        printf(found ? "Hit\n" : "Fault\n");
+    }
+
+    printf("Hits = %d\nFaults = %d\n", hit, fault);
+}
+
+void lru() {
+    int page[] = {9,8,0,8,7,6,7,5,3,7};
+    int frame[4] = {-1,-1,-1,-1};
+    int time[4] = {0};
+    int i, j, found, pos, hit = 0, fault = 0;
+
+    printf("\nLRU Page Replacement\n");
+
+    for(i = 0; i < 10; i++) {
+        found = 0;
+
+        for(j = 0; j < 4; j++) {
+            if(frame[j] == page[i]) {
+                found = 1;
+                hit++;
+                time[j] = i;
             }
         }
 
-        if(found == 0) {
-            memory[pointer] = page[i];
-            pointer = (pointer + 1) % frames;
-            faults++;
+        if(!found) {
+            pos = 0;
+            for(j = 1; j < 4; j++)
+                if(time[j] < time[pos])
+                    pos = j;
+
+            frame[pos] = page[i];
+            time[pos] = i;
+            fault++;
         }
 
-        printf("Page %d -> ", page[i]);
+        printf("%d -> ", page[i]);
+        for(j = 0; j < 4; j++)
+            printf("%d ", frame[j]);
 
-        for(j = 0; j < frames; j++) {
-            if(memory[j] == -1)
-                printf("- ");
-            else
-                printf("%d ", memory[j]);
-        }
-
-        if(found)
-            printf("Hit\n");
-        else
-            printf("Fault\n");
+        printf(found ? "Hit\n" : "Fault\n");
     }
 
-    printf("\nTotal Hits   : %d\n", hits);
-    printf("Total Faults : %d\n", faults);
+    printf("Hits = %d\nFaults = %d\n", hit, fault);
+}
 
+int main() {
+    fifo();
+    lru();
     return 0;
 }
